82d184a7ccf2159906216438ef5747409be6ce25
omar-faruk-6 <68624414+omar-faruk-6@users.noreply.github.com>
2022-09-19 12:44:22 +0600

    Add files via upload

---
+++ FTtimeFuntionAperiodicPluse.m
@@ -0,0 +1,22 @@
+clc;
+clear all;
+close all;
+f=-2:.01:2;
+x=4*sinc(4*pi*f);
+
+%real part
+subplot(3,1,1);
+plot(f,real(x));
+title('Real Part');
+xlabel('frequency');
+%Magnitude part
+subplot(3,1,2)
+plot(f,abs(x));
+title('Magnitude part');
+xlabel('Frequency');
+
+%Phase part
+subplot(3,1,3);
+plot(f,angle(x));
+title('Phase part');
+xlabel('Frequency');
